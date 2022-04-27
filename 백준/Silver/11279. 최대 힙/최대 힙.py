77b19547271db8f8f8dec46e2533736a87ed3481
hyosunLee <leecrossun@naver.com>
2022-04-28 00:46:30 +0900

    [Silver II] Title: 최대 힙, Time: 184 ms, Memory: 45980 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,14 @@
+# 최대 힙
+import heapq, sys
+N = int(input())
+heap = []
+
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if len(heap) == 0:
+            print("0")
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (-x, x))
